a7874402013affdc189a5aec6d172075d0fd2987
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-01-17 11:39:46 -0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,48 @@
+import { getUser } from "./services/user.js"
+import { getRepositories } from "./services/repositories.js"
+import { user } from "./objects/user.js"
+import { screen } from "./objects/screen.js"
+
+
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value.trim()
+    if (validateEmptyInput(userName)) return
+    getUserData(userName)
+})
+
+document.getElementById('input-search').addEventListener('keyup', (e) => {
+    const userName = e.target.value.trim()
+    const key = e.which || e.keyCode //pegando o código da chave
+    const isEnterKeyPressed = key === 13
+
+    if (isEnterKeyPressed) {
+        if (validateEmptyInput(userName)) return
+        getUserData(userName)
+    }
+})
+
+function validateEmptyInput(userName) {
+    if (userName.length === 0 || userName === '') {
+        alert('Preencha o campo com o nome do usuário do GitHub')
+        return true
+    }
+}
+
+async function getUserData(userName) {
+    const userResponse = await getUser(userName)
+    const repositoriesResponse = await getRepositories(userName)
+
+    if (userResponse.message === 'Not Found') {
+        screen.renderNotFound()
+        return
+    }
+
+    user.setInfo(userResponse)
+    user.setRepositories(repositoriesResponse)
+
+    screen.renderUser(user)
+}
+
+
+
+
---
+++ js/services/user.js
@@ -0,0 +1,10 @@
+import { baseUrl } from "../variables.js"
+
+async function getUser(userName) {
+    const url = `${baseUrl}/${userName}`
+    const response = await fetch(url)
+    return await response.json()
+}
+
+export { getUser }
+
